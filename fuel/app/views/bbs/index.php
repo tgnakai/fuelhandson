63bf605cc9272a5af5779407c2a35a8ebf628054
tgnakai <tsnakai@gmail.com>
2013-07-20 17:11:49 +0900

    update singer bug

--- fuel/app/views/bbs/index.php
+++ fuel/app/views/bbs/index.php
@@ -10,14 +10,14 @@
 		</tr>
 	</thead>
 	<tbody>
-<?php foreach ($bbs as $bbs): ?>		<tr>
+<?php foreach ($bbs as $data): ?>		<tr>
 
-			<td><?php echo $bbs->post_date; ?></td>
-			<td><?php echo $bbs->message; ?></td>
+			<td><?php echo $data->post_date; ?></td>
+			<td><?php echo $data->message; ?></td>
 			<td>
-				<?php echo Html::anchor('bbs/view/'.$bbs->id, '<i class="icon-eye-open" title="View"></i>'); ?> |
-				<?php echo Html::anchor('bbs/edit/'.$bbs->id, '<i class="icon-wrench" title="Edit"></i>'); ?> |
-				<?php echo Html::anchor('bbs/delete/'.$bbs->id, '<i class="icon-trash" title="Delete"></i>', array('onclick' => "return confirm('Are you sure?')")); ?>
+				<?php echo Html::anchor('bbs/view/'.$data->id, '<i class="icon-eye-open" title="View"></i>'); ?> |
+				<?php echo Html::anchor('bbs/edit/'.$data->id, '<i class="icon-wrench" title="Edit"></i>'); ?> |
+				<?php echo Html::anchor('bbs/delete/'.$data->id, '<i class="icon-trash" title="Delete"></i>', array('onclick' => "return confirm('Are you sure?')")); ?>
 
 			</td>
 		</tr>
