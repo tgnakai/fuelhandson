41f4f171bd6f37db64021546afac30fc57d78489
louklouk <louk.louk+git@gmail.com>
2013-07-20 15:13:06 +0900

    add showdata

--- fuel/app/classes/controller/demo.php
+++ fuel/app/classes/controller/demo.php
@@ -4,5 +4,10 @@ class Controller_Demo extends Controller
     public function action_index() {
         return Response::forge( View::forge('demo/index') );
     }
+    
+    public function action_showdata() {
+        $data = array('text' => 'Nagoya');
+        return Response::forge( View::forge('demo/showdata', $data) );
+    }
 }
 ?>
