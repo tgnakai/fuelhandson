e0331d28953a76880d5f804101f3c10a94bb406d
tgnakai <tsnakai@gmail.com>
2013-07-20 15:28:03 +0900

    add function select

--- fuel/app/classes/controller/demo.php
+++ fuel/app/classes/controller/demo.php
@@ -9,5 +9,19 @@ class Controller_Demo extends Controller
         $data = array('text' => 'Nagoya');
         return Response::forge( View::forge('demo/showdata', $data) );
     }
+    
+    public function action_select($id = null) {
+        $list = array(
+            1 => '1st',
+            2 => '2nd',
+            3 => '3rd'
+        );
+        if ($id === null) $id = 1;
+        if ($id > 3) $id = 3;
+        $data = array(
+            'text' => $list[$id]
+        );
+        return Response::forge( View::forge('demo/select', $data) );
+    }
 }
 ?>
